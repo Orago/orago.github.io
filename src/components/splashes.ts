9a2f4040069099bb626c74645f26edb2a28f49ae
Orago <moshlingcat2004@gmail.com>
2024-09-19 15:55:44 -0400

    neocity port

--- src/components/splashes.ts
+++
@@ -1,8 +0,0 @@
-const splashes: Array<string> = [
-	'Now with 50% more unpaid cat labor',
-	'All on Me Own 😔',
-	'Check out you MyTube'
-];
-
-
-export default splashes;
--- src/sharedUtil.ts
+++
@@ -1,4 +0,0 @@
-import { ProxyNode, qs } from '@orago/dom';
-
-export const mainNode: ProxyNode = qs('#main') as ProxyNode;
-export const bodyNode: ProxyNode = qs('body') as ProxyNode;
